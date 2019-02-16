05a7515e00eaa02359292f998512176dc6082b5f
Bobbi York <york1670@gmail.com>
2019-02-16 23:01:17 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,3 +1,4 @@
+require 'nokogiri'
 require 'pry'
 
 html = File.read('fixtures/kickstarter.html')
@@ -6,5 +7,6 @@ kickstarter = Nokogiri::HTML(html)
 
 def create_project_hash
   # write your code here
+  # projects: kickstarter.css("li.project.grid_4")
+  binding.pry
 end
-binding.pry
